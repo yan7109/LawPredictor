70c761373e1c9f0e4f2b98c72469c96d412b8324
Tianyi Wang <wang103uiuc@gmail.com>
2014-11-13 02:17:59 -0800

    Fixed the path in main since I changed the relative path meaning

--- Project/main.py
+++ Project/main.py
@@ -1,16 +1,21 @@
-import sys, os, json
+import json
+import os
+import sys
 sys.path.append('FeatureExtractor/')
 sys.path.append('Model/')
 import feature_extractor
 from svmModel import *
 
+
 # To start, must create a FeatureExtractor object.
 fe = feature_extractor.FeatureExtractor()
+
 # You can pick which categories you want to include in the output.
 fe.include_all_categories()
 fe.change_section_weight(feature_extractor.INDEX_SEC_HELD, 0)
 fe.change_section_weight(feature_extractor.INDEX_SEC_DISCUSSION, 0)
-cases_relative_path = os.path.join('..', 'Cases')
+
+cases_relative_path = 'Cases'
 features = fe.compute_word_weights_to_hold_result(cases_relative_path)
 
 a = svmModel(features)
